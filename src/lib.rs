9cb8ed0ab0dad1aa4cca799b1898d3ca93ee09a2
Robert Lillack <rob@lillack.net>
2025-10-13 17:58:23 +0200

    Move markdown parser stuff to richtext

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 // Library exports for fliki-rs
 
+pub mod content;
 pub mod fltk_structured_rich_display;
 pub mod fltk_text_display;
-pub mod content;
-pub mod page_ui;
-pub mod ui_adapters;
 pub mod link_handler;
-pub mod markdown_ast;
-pub mod markdown_parser;
+pub mod page_ui;
 pub mod responsive_scrollbar;
 pub mod richtext;
 pub mod sourceedit;
+pub mod ui_adapters;
